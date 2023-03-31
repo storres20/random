774a09e0495752378e029fe364c79cd730807f64
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-30 19:02:55 -0500

    Update nationalities page

--- src/pages/Nationalities.tsx
+++ src/pages/Nationalities.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import styles from './Nationalities.module.css'
 
 export const Nationalities = () => {
+
+  interface Data {
+    nat: string;
+  }
+  
+  /* Set useState */
+  const [data, setData] = useState<Data[] | null>(null);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?inc=nat&results=3')
+      .then(response => {
+        console.log(response.data.results)
+        setData(response.data.results);
+        setError(null);
+      })
+      .catch(error => {
+        setError(error.message);
+        setData(null);
+      });
+  }, []);
+
+
   return (
-    <div>Nationalities - a list of nationalities (just 3) selected from the available list</div>
+    <div>
+      <h2>Please select a Nationality</h2>
+      
+      {
+        data && data.map((item, index) => (
+          <Link to={`/users/nationality/${item.nat}`}><p key={index} className={styles.link}>{item.nat}</p></Link>
+        ))
+      }
+    
+    </div>
+    
+    
   )
 }
+
