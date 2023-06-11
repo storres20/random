66220f83d34e42c114a41badc8151265f4d21ae2
Italo Lon kan <sanchotorres777@gmail.com>
2023-06-11 11:19:13 -0500

    Update Navbar component with tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import { Navbar } from "./components/Navbar";
-import { Home } from './pages/Home';
-import { Nationalities } from './pages/Nationalities';
-import { Users } from './pages/Users';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { Home } from './pages/Home'
+import { Nationalities } from './pages/Nationalities'
+import { Users } from './pages/Users'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
-      
+      <Navbar />
+
       <Routes>
-        <Route path="/" element={<Home/> } />
-        <Route path="/users" element={<Nationalities/> } />
-        <Route path="/users/nationality/:id" element={<Users/> } />
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/users"
+          element={<Nationalities />}
+        />
+        <Route
+          path="/users/nationality/:id"
+          element={<Users />}
+        />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,108 @@
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-export const Navbar = () => {
+export function Navbar() {
+  const [mobile, setMobile] = useState(false)
+
+  const handleMobile = () => {
+    setMobile(!mobile)
+  }
+
   return (
-    <>
-      <nav className="navbar navbar-expand-md bg-dark" data-bs-theme='dark'>
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">Home</NavLink>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <NavLink className="nav-link" to="/users">Nationalities</NavLink>
+    <div>
+      {/* navbar goes here */}
+      <nav className="bg-gray-100">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="flex justify-between">
+            <div className="flex space-x-4">
+              {/* logo */}
+              <div>
+                <Link
+                  to="/"
+                  className="flex items-center space-x-1 py-4 text-gray-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 text-blue-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 002.25-2.25V6.75a2.25 2.25 0 00-2.25-2.25H6.75A2.25 2.25 0 004.5 6.75v10.5a2.25 2.25 0 002.25 2.25zm.75-12h9v9h-9v-9z"
+                    />
+                  </svg>
+                  <span className="font-bold">RandomUser</span>
+                </Link>
+              </div>
+              {/* primary nav */}
+              <div className="hidden md:flex items-center space-x-1">
+                <Link
+                  to="/"
+                  className="py-4 px-3 text-gray-700 hover:text-gray-900"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/users"
+                  className="py-4 px-3 text-gray-700 hover:text-gray-900"
+                >
+                  Nationalities
+                </Link>
+              </div>
+            </div>
+
+            {/* mobile button goes here */}
+            <div
+              className="md:hidden flex items-center"
+              onClick={handleMobile}
+            >
+              <button aria-label="button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
+        {/* mobile menu */}
+        <div className={`${mobile ? '' : 'hidden '} md:hidden`}>
+          <Link
+            to="/"
+            className="block py-2 px-4 text-sm hover:bg-gray-200"
+            onClick={handleMobile}
+          >
+            Home
+          </Link>
+          <Link
+            to="/users"
+            className="block py-2 px-4 text-sm hover:bg-gray-200"
+            onClick={handleMobile}
+          >
+            Nationalities
+          </Link>
+        </div>
       </nav>
-    </>
+    </div>
   )
 }
+
+/* youtube tutorial
+https://www.youtube.com/watch?v=miiPsBlqMns&t=1473s
+Building Tailwind CSS Navbars - Responsive Too!
+DigitalOcean channel
+*/
